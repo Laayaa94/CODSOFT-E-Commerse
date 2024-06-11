ae4432dfaffdd8a1a2dc27b7f44ebff899a67335
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-06-11 17:33:31 +0530

    add banners to app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
+
 function App() {
   return (
     <div className="App">
@@ -13,9 +17,9 @@ function App() {
       <NavBar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCatogory category="men"/>}/>
-        <Route path='/womens' element={<ShopCatogory category="women"/>}/>
-        <Route path='/kids' element={<ShopCatogory category="kid"/>}/>
+        <Route path='/mens' element={<ShopCatogory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCatogory banner={women_banner}category="women"/>}/>
+        <Route path='/kids' element={<ShopCatogory banner={kid_banner}category="kid"/>}/>
           <Route path="/product" element={<Product/>}>
             <Route path=':productId' element={<Product/>}/>
           
--- src/Pages/ShopCatogory.jsx
+++ src/Pages/ShopCatogory.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCatogory.css'
+import all_product from '../Components/Assets/all_product'
+import { ShopContext } from '../Context/ShopContext/ShopContext'
 
-const ShopCatogory = () => {
+
+const ShopCatogory = (props) => {
+  const{all_product}=useContext(ShopContext);
   return (
-    <div>
+    <div className='shop-catogory'>
       
     </div>
   )
