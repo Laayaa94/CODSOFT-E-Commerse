8f7b9fbc5d28d43803b2e4d52f5383e7820ce2ca
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-06-12 21:50:06 +0530

    Initial Breadcrum

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './Item.css'
+import { Link } from 'react-router-dom';
 const Item = (props) => {
   return (
     <div className='item'>
-      <img src={props.image} alt="" />
+      <Link to={`/product/${props.id}`}><img src={props.image} alt="" /></Link>
       <p>{props.name}</p>
       <div className="itemPrices">
         <div className="itemPriceNew">
             ${props.new_price}
         </div>
         <div className="itemPriceOld">
-            {props.old_price}
+            ${props.old_price}
         </div>
       </div>
     </div>
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react'
-import {ShopContext} from '../Context/ShopContext'
+import {ShopContext} from '../Context/ShopContext/ShopContext'
 import { useParams } from 'react-router-dom';
+import Breadcrums from '../Components/Breadcrums/Breadcrums';
 const Product = () => {
   const {all_product}=useContext(ShopContext);
   const {productId} =useParams();
   const product=all_product.find((e)=>e.id=== Number(productId));
   return (
     <div>
-      
+      <Breadcrums product={product}/>
     </div>
   )
 }
