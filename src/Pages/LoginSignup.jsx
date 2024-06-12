3b86065c49b521380a6beda6be2dafac34bbc606
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-06-12 21:03:09 +0530

    initial commit for login page

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const LoginSignup = () => {
   return (
-    <div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email'/>
+          <input type="password" placeholder='Password' />
+
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? 
+          <span>Login here</span>
+          <div className="loginsignuop-agree">
+            <input type="checkbox" name='' id=''/>
+            <p>By continuing, I agree to the terms & privacy policy</p>
+          </div>
+        </p>
+      </div>
+
       
     </div>
   )
